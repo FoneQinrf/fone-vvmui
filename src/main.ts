1487e112b64e830f4582546d6588dd5e2f932e94
qinrifeng <15921712019@163.com>
2021-05-11 15:26:56 +0800

    补全文档

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: Fone`峰
  * @Date: 2021-04-02 11:27:52
  * @LastEditors: Fone`峰
- * @LastEditTime: 2021-05-09 17:16:27
+ * @LastEditTime: 2021-05-11 15:26:38
  * @Description: file content
  * @Email: qinrifeng@163.com
  * @Github: https://github.com/FoneQinrf
@@ -11,11 +11,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/router';
 import './style/index.scss';
-import Code from './components/Code.vue';
+// import Code from './components/Code.vue';
 import vvmui from "@/vvmui";
 
 const app = createApp(App);
 app.use(vvmui);
-app.component('Code', Code);
+// app.component('Code', Code);
 
 app.use(router).mount('#app');
